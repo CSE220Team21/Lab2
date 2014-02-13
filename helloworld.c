d337f2656da9c46025fa86df33e14ea88390ae4f
Mike Mayr <mmayr@asu.edu>
2014-02-12 19:31:18 -0700

    edited spelling mistake in comments

--- helloworld.c
+++ helloworld.c
@@ -1,10 +1,9 @@
 /* Hello World program */
-//Hey...i'm commenting on this assignment
 
 #include<stdio.h>
 
 main()
 {
-    printf("Hello World"); /* Prints Hello World */
+    printf("Hello World"); /* Prints Hello World to screen*/
 
 }
