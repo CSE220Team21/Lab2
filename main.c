ecd896b0a7de245f62b195edd0418f6d3669019b
alkruege <alkruege@asu.edu>
2014-02-21 09:42:54 -0700

    boolean function
    Calls the boolean function in main and finishes the if statements in the boolean function.

--- main.c
+++ main.c
@@ -8,7 +8,8 @@ int main (int argc, const char *argv[])
     
     source_file=init_lister(*argv,source_name,date) //Calls the init lister function and sets a value to the pointer source_file
     
-   /* Missing code here */
+   while(get_source_line(source_file,source_name,date)){
+   }//Calls the Boolean function until it is false
     return 0;
 }
 FILE *init_lister(const char *name, char source_file_name[], char dte[])
@@ -25,9 +26,11 @@ BOOLEAN get_source_line(FILE *src_file, char src_name[], char todays_date[])
     char source_buffer[MAX_SOURCE_LINE_LENGTH];
     static int line_number = 0;
     
-    if (1) //This is missing a condition
+    if (source_buffer[0]) //This is the condition for the if statement
     {
-		/*  Missing Code Here */
+	strcpy(print_buffer,source_buffer);//copies the string in source buffer over to print buffer
+	line_number++;//increments the line number
+	print_line(print_buffer,src_name,todays_date); //calls the print line function
         return (TRUE);
     }
     else
