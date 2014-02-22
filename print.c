a5cc0caf26d66a2077c253fa772b465d178e6265
alkruege <alkruege@asu.edu>
2014-02-22 14:06:52 -0700

    FINAL print.c

--- print.c
+++ print.c
@@ -4,38 +4,31 @@
 
 static void print_page_header(char source_name[], char date[]);
 
-void print_line(char line[], char source_name_to_print[], char date_to_print[])
+void print_line(int lineNumber, char line[], char source_name_to_print[], char date_to_print[])
 {
-    char save_ch;
-    char *save_chp = NULL;
-    static int line_count = MAX_LINES_PER_PAGE;
-    
-    if (++line_count > MAX_LINES_PER_PAGE)
-    {
-        print_page_header(source_name_to_print, date_to_print);//calls the print page header function
-        line_count=1;//initializes the line count to 1
-    }
-    if (strlen(line) > MAX_PRINT_LINE_LENGTH) 
-    {
-        save_chp=(line+MAX_PRINT_LINE_LENGTH);//sets save chp to the line plus the length
-        save_ch=*save_chp;//sets save ch to the pointer
-        *save_chp='\0';//dereferences the pointer and sets it equal to the null character
-    }
-    if (save_chp)  
-    {
-        printf("%s", line);//prints the line of source code
-        *save_chp=save_ch;//sets the pointer back
-    }
-   
-    if (save_chp)
-    {
-       line_count++;  //increments line_count
-    }
+     char save_ch;
+     char *save_chp = NULL;
+     static int line_count = MAX_LINES_PER_PAGE;
+
+     if ((lineNumber%MAX_LINES_PER_PAGE) ==  0)
+     {
+          print_page_header(source_name_to_print, date_to_print);//calls the print page header function
+          line_count=1;//initializes the line count to 1
+     }
+     if (strlen(line) > MAX_PRINT_LINE_LENGTH) 
+     {
+          save_chp=(line+MAX_PRINT_LINE_LENGTH);//sets save chp to the line plus the length
+          save_ch=*save_chp;//sets save ch to the pointer
+          *save_chp='\0';//dereferences the pointer and sets it equal to the null character
+     }
+     printf("%d: %s", lineNumber, line);//prints the line number
+     line_count++;  //increments line_count
 }
 static void print_page_header(char source_name[], char date[])
 {
-    static int page_number = 0;
-    
-    page_number++;//increments page number
-    printf("%s\nPage: %d %s\n", source_name, page_number, date);//calls printf and prints out the header with the file name, page number, and the date
+     static int page_number = 0;
+
+     page_number++;//increments page number
+     printf("\n%s Page: %d %s\n", source_name, page_number, date);//calls printf and prints out the header with the file name, page number, and the date
 }
+  
